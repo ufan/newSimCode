ae953441fd362f7990d69518651fe8e8099558c6
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-10-26 17:00:46 +0100

    modified:   Analysis.C

--- AlgSimu/share/Analysis.C
+++ AlgSimu/share/Analysis.C
@@ -1,5 +1,5 @@
 /*
- *  $Id: Analysis.C, 2014-10-22 15:10:35 DAMPE $
+ *  $Id: Analysis.C, 2014-10-26 11:03:38 DAMPE $
  *  Author(s):
  *    Chi WANG (chiwang@mail.ustc.edu.cn) 22/10/2014
  *
@@ -28,12 +28,12 @@ void VertexOfPhoton(){
   TCanvas *c1 = new TCanvas("VertexOfPhoton","VertexOfPhoton");
   c1->Divide(2,2);
   c1->cd(1);
-  eventTree->Draw("TrackVertex.fPosition.fZ","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)");
+  eventTree->Draw("TrackVertex.fPosition.fZ","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)&&(TrackVertex.fPosition.fZ < -5000)");
   c1->cd(2);
-  eventTree->Draw("TrackVertex.fDirection.Theta()","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)");
+  eventTree->Draw("TrackVertex.fDirection.Theta()","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)&&(TrackVertex.fPosition.fZ < -5000)");
   c1->cd(3);
-  eventTree->Draw("TrackVertex.fEnergy:TrackVertex.fPosition.fZ","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)");
+  eventTree->Draw("TrackVertex.fEnergy:TrackVertex.fPosition.fZ","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)&&(TrackVertex.fPosition.fZ < -5000)");
   c1->cd(4);
-  eventTree->Draw("TrackVertex.fDirection.Theta():TrackVertex.fEnergy","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)");
+  eventTree->Draw("TrackVertex.fDirection.Theta():TrackVertex.fEnergy","(TrackVertex.fPDGCode==22)&&(TrackVertex.fParentID == 1)&&(TrackVertex.fPosition.fZ < -5000)");
 }
 
